ac6fc5f02444820edbf479b633b8c1351c4b4e19
KaranRohra <rohrakaran38@gmail.com>
2024-07-31 14:45:28 +0530

    Connected with MongoDB

--- backend/src/service.ts
+++ backend/src/service.ts
@@ -1,36 +1,61 @@
-import { CryptoCurrency } from "./type";
+import { Collection, Db, MongoClient, ServerApiVersion } from 'mongodb';
+import { CryptoCurrency } from './type';
+
+const uri = 'mongodb+srv://root:Fa7tblgupLkh82fe@mongodb.kwdr3ys.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB';
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 const fetchCryptoCurrencyDetails = async (): Promise<CryptoCurrency[]> => {
   try {
     const response = await fetch('https://api.livecoinwatch.com/coins/list', {
       body: JSON.stringify({
-        "currency": "USD",
-        "sort": "rank",
-        "order": "ascending",
-        "offset": 0,
-        "limit": 5
+        currency: 'USD',
+        sort: 'rank',
+        order: 'ascending',
+        offset: 0,
+        limit: 5,
       }),
       headers: {
         'content-type': 'application/json',
-        'x-api-key': '5720bf4a-72db-4bb1-9c18-e3d2c642d49f'
+        'x-api-key': '5720bf4a-72db-4bb1-9c18-e3d2c642d49f',
       },
       method: 'POST',
-    })
-    return await response.json()
+    });
+    return await response.json();
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return [];
   }
-}
+};
+
+export const mongoDbOperations = async (opFuncName: keyof Collection<Document>, params: any | null) => {
+  try {
+    await client.connect();
+    const db: Db = client.db('fomo-factory');
+    const collection: any = db.collection('crypto-currency');
+    if (params === null) return await collection[opFuncName]();
+
+    return await collection[opFuncName](params);
+  } catch (err) {
+    console.error('An error occurred:', err);
+  } finally {
+    await client.close();
+  }
+};
 
 export const updateCryptoCurrencyDetails = async () => {
-  const cryptoCurrencies = await fetchCryptoCurrencyDetails()
-  // Add data to MongoDB
-  cryptoCurrencies.forEach(cryptoCurrency => {
-    console.log(cryptoCurrency.code)
-  })
-  console.log(`[${new Date()}]: Updated crypto currency details`)
+  const cryptoCurrencies = await fetchCryptoCurrencyDetails();
+  await mongoDbOperations(
+    'insertMany',
+    cryptoCurrencies.map((cc) => ({ ...cc, fetchedAt: Date.now() }))
+  );
+
+  console.log(`[${new Date()}]: Updated crypto currency details`);
 
   setTimeout(updateCryptoCurrencyDetails, 5000);
-}
-
+};
--- backend/src/type.ts
+++ backend/src/type.ts
@@ -1,7 +1,7 @@
 export type CryptoCurrency = {
-  code: string,
-  rate: number,
-  volume: number,
-  cap: number,
-  timestamp?: number
-}
+  code: string;
+  rate: number;
+  volume: number;
+  cap: number;
+  fetchedAt: number;
+};
